b847e3e753356f6cf391740748cf86e0a2c3dec4
Martin C <info@d-modules.com>
2016-12-05 20:11:57 -0500

    delete uncessary files

--- fabfile.py
+++ fabfile.py
@@ -1,25 +1,22 @@
-from fabric.api import local
+from fabric.api import local, put, hosts, env, run, cd
 
-def test():
-    env.user = 'testuser'
-    env.hosts = ['test.server.com']
-
-def live():
-    env.user = 'testuser'
-    env.hosts = ['test.server.com']
+host = '54.165.172.158'
+env.hosts = [host]
+env.user = "ec2-user"
+env.key_filename = "LightsailDefaultPrivateKey.pem"
 
 def uploadbackup():
-    print("Starting backup db")
-    local('python manage.py dumpdata > ./data/backups/db.json')
-    upload = put(remote_path="./data/backups/db.json", local_path="/home/data/backups/db.json")
-    upload.succeeded
-    run('docker-compose run web python manage.py loaddata /data/backups/db.json')
-    print("Backup db finish")
+    code_dir = '/home/ec2-user/project'
+    with cd(code_dir):
+        local('pg_dump -U postgres -h postgres postgres > ./data/backups/dbexport.pgsql')
+        upload = put(local_path="./data/backups/dbexport.pgsql", remote_path="/home/ec2-user/project/data/backups/dbexport.pgsql")
+        upload.succeeded
+        run('docker-compose run web psql -h postgres -d postgres -U postgres -f /home/ec2-user/project/data/backups/dbexport.pgsql')
 
 def downloadbackup():
     print("Starting restore db")
     run('python manage.py dumpdata > ./data/backups/db.json')
-    download = get(remote_path="/home/data/backups/db.json", local_path="./data/backups/db.json")
+    download = get(remote_path="/home/ec2-user/project/data/backups/db.json", local_path="./data/backups/db.json")
     download.succeeded
     local('python manage.py loaddata /data/backups/db.json')
     print("Restore db finish")
