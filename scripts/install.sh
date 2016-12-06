cee75ec80ac72c62557f65f277d3a89681f1d598
Martin C <info@d-modules.com>
2016-12-05 19:25:13 -0500

    scripts

---
+++ scripts/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# stop on errors
+
+echo 'deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main' >/etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+apt-get update
+apt-get -y install postgresql-client
