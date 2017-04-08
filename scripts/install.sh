ccce33e272d7c089605198b2d708074d71db9627
martcot <mcote@d-modules.com>
2017-04-08 00:09:07 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,5 +5,6 @@ echo 'deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main' >/etc/apt/s
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt-get update
 apt-get -y install postgresql-client
+apt-get -y install gunicorn
 
 apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
